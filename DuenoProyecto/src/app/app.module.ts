ea5f203045dc65519cc7b4ac44a583b67630caaf
RobertBZ <rbalderrama41@gmail.com>
2020-03-25 17:07:23 -0400

    Generaro de pagina de login

--- DuenoProyecto/src/app/app.module.ts
+++ DuenoProyecto/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FirebaseModule } from './firebase';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     FirebaseModule,
