b598593ed7aef9464c7ddc17890bc64123b41582
Rodriv01 <rodrigo.797.vargas@gmail.com>
2020-03-25 12:04:31 -0400

    Añadida conexion a Firebase

--- ClienteProyecto/src/app/app.module.ts
+++ ClienteProyecto/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FirebaseModule,
     BrowserModule,
     AppRoutingModule
   ],
