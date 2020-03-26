a554ea4c1a1c1b068af1183be70b32f6db0236ba
Rodriv01 <rodrigo.797.vargas@gmail.com>
2020-03-26 11:08:16 -0400

    Añadida funcionalidad en db service

--- ProyectoCliente/src/app/models/IUsage.ts
+++ ProyectoCliente/src/app/models/IUsage.ts
@@ -1,9 +1,37 @@
 import { IModel } from './IModel';
 
 export interface IUsage extends IModel{
-    
+    Amount: number;
+    ClientID: string;
+    Name: string;
+    OrderDate: Date;
+    Photo: string;
+    Price: number;
 }
 
 export class Usage implements IUsage {
-    
+    Id?: string;
+    Amount: number;
+    ClientID: string;
+    IsDeleted: boolean = false;
+    Name: string;
+    OrderDate: Date;
+    Photo: string;
+    Price: number;
+
+    constructor( 
+        amount: number, 
+        clientID: string, 
+        name: string, 
+        orderDate: Date, 
+        photo: string, 
+        price: number
+    ){
+        this.Amount = amount;
+        this.ClientID = clientID;
+        this.Name = name;
+        this.OrderDate = orderDate;
+        this.Photo = photo;
+        this.Price = price;
+    }
 }
