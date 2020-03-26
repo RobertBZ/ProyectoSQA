cda1f9527d2157f997f466de0401ddf2895df118
RobertBZ <rbalderrama41@gmail.com>
2020-03-26 11:14:25 -0400

    Generado del page clientes y creado del dashboard

--- ProyectoDueno/src/app/app-routing.module.ts
+++ ProyectoDueno/src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { CustomersPageComponent } from './pages/customers-page/customers-page.component';
 
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'customers',
+    component: CustomersPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
   }
 ];
 
