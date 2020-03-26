3937527c74f50742a43672cfb509867a3c0bb8c5
Rodriv01 <rodrigo.797.vargas@gmail.com>
2020-03-26 11:09:29 -0400

    Añadida funcionalidad en db service

--- ProyectoCliente/src/app/services/db.service.ts
+++ ProyectoCliente/src/app/services/db.service.ts
@@ -16,14 +16,7 @@ export class DBService {
   createDocumentWithID<T extends IModel>(collectionName : string, element: T, elementID : string){
     return this.firestore.collection<T>(collectionName).doc(elementID).set(element);
   }
-  updateDocument<T extends IModel>(collectionName: string, id: string, data : T){
-    return this.firestore.doc<T>(collectionName + '/' + id).update({...data});
-  }
-  deleteDocument<T extends IModel>(collectionName : string, data : T){
-    data.IsDeleted = true;
-    return this.updateDocument<T>(collectionName, data.Id, data);
-  }
-  getDocuments<T extends IModel>(collectionName : string) {
+  readDocuments<T extends IModel>(collectionName : string) {
     return this.firestore.collection<T>(collectionName).snapshotChanges().pipe(map(data => {
       return data.map(e => {
         const Id = e.payload.doc.id;
@@ -31,6 +24,13 @@ export class DBService {
         return { Id, ...t };
       }).filter(element => !element.IsDeleted);
     }));
+  }
+  updateDocument<T extends IModel>(collectionName: string, id: string, data : T){
+    return this.firestore.doc<T>(collectionName + '/' + id).update({...data});
+  }
+  deleteDocument<T extends IModel>(collectionName : string, data : T){
+    data.IsDeleted = true;
+    return this.updateDocument<T>(collectionName, data.Id, data);
   }
 
 }
