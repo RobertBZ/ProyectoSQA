a9e28655c1bfc12fcbe3f049f9d9f7c89436a61d
Rodriv01 <rodrigo.797.vargas@gmail.com>
2020-03-26 11:47:16 -0400

    Añadida funcionalidad en usage service

--- ProyectoCliente/src/app/services/usage.service.ts
+++ ProyectoCliente/src/app/services/usage.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Usage, IUsage } from './../models/IUsage';
+import { IUsage } from './../models/IUsage';
 import { DBService } from './db.service';
 
 @Injectable({
@@ -7,8 +7,23 @@ import { DBService } from './db.service';
 })
 export class UsageService {
 
+  collectionName : string = 'Usage';
+
   constructor(private dbService : DBService) { }
 
-  
+  createUsage( usage : IUsage ) {
+    return this.dbService.createDocument<IUsage>( this.collectionName, usage );
+  }
 
+  readUsage() {
+    return this.dbService.readDocuments<IUsage>( this.collectionName );
+  }
+
+  updateUsage( usage : IUsage ) {
+    return this.dbService.updateDocument<IUsage>( this.collectionName, usage );
+  }
+
+  deleteUsage( usage : IUsage ){
+    return this.dbService.deleteDocument(this.collectionName, usage );
+  }
 }
