ce10a6ad5023ec62a9391733e693755e02c1ac2b
RobertBZ <rbalderrama41@gmail.com>
2020-03-27 17:37:15 -0400

    Formulario de cliente funcionando

--- ProyectoDueno/src/app/modals/customer-modal/customer-modal.component.ts
+++ ProyectoDueno/src/app/modals/customer-modal/customer-modal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/services/customer.service';
+import { Client } from 'src/app/models/IClient';
 
 @Component({
   selector: 'app-customer-modal',
@@ -7,9 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerModalComponent implements OnInit {
 
-  constructor() { }
+  clientForm: any;
+
+  constructor(
+    private customerService: CustomerService
+  ) {
+    this.clientForm = new FormGroup({
+      Email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9-]+\x2e[a-z]{2,4}')]),
+      Password: new FormControl('', [Validators.required]),
+      Name: new FormControl('', [Validators.required]),
+      Lastname: new FormControl('', [Validators.required]),
+      Username: new FormControl('', [Validators.required]),
+      Credit: new FormControl('', [Validators.required]),
+    });
+  }
+
+  get Name() {
+    return this.clientForm.get("Name");
+  }
+
+  get Lastname() {
+    return this.clientForm.get("Lastname");
+  }
+  get Email() {
+    return this.clientForm.get("Email");
+  }
+  get Username() {
+    return this.clientForm.get("Username");
+  }
+  get Credit() {
+    return this.clientForm.get("Credit");
+  }
+  get Password() {
+    return this.clientForm.get("Password");
+  }
 
   ngOnInit() {
+  }
+
+  createClient() {
+    if (this.clientForm.status === 'VALID') {
+      var client = { 
+        Email: this.Email.value,
+        Lastname: this.Lastname.value,
+        Name: this.Name.value, 
+        Username: this.Username.value, 
+        IsDeleted : false, 
+        Credit: this.Credit.value
+      };
+      this.customerService
+        .createClient( client, this.Password.value).then(res => {
+          console.log("se ah registrado exitosamente");
+        }).catch(err => {
+          console.log("Ocurrio un error en al registrar");
+        })
+    }
+    else {
+      this.Email.touched = true;
+      this.Name.touched = true;
+      this.Lastname.touched = true;
+      this.Username.touched = true;
+      this.Password.touched = true;
+      this.Credit.touched = true;
+      console.log("error")
+    }
   }
 
 }
